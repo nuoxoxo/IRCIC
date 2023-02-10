3226b3c67bc24bf4ab6c8555262340bcd896a333
nuoxoxo <malcol.nuo.xu@gmail.com>
2023-02-10 08:59:49 +0100

    fix . now has db has wp at the bottom

--- inception/snapshot/snapshpot_8/project/srcs/requirements/mariadb/conf/create_db.sh
+++ inception/snapshot/snapshpot_8/project/srcs/requirements/mariadb/conf/create_db.sh
@@ -23,11 +23,12 @@ DROP DATABASE test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
-CREATE DATABASE '${DB_NAME}' CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASS}';
 GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
+
         # run init.sql
         /usr/bin/mysqld --user=mysql --bootstrap < /tmp/db_autogen_command.sql
         rm -f /tmp/db_autogen_command.sql
