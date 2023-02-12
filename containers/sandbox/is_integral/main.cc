6e74aa5daf572dd44f0d7d8ceca7970e3a4b7399
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-02-12 19:23:51 +0100

    + is integral sandbox

--- containers/sandbox/is_integral/main.cc
+++ containers/sandbox/is_integral/main.cc
@@ -1,36 +1,39 @@
-#include <iostream>
-#include <iomanip>
-#include <type_traits>
+#include "iostream"
+#include "type_traits"
+#define nl "\n"
+#define nl2 "\n\n"
+
+using	namespace std;
  
-class A {};
- 
-struct B { int x:4{2}; };
-using BF = decltype(B::x); // bit-field's type
- 
-enum E : int {};
- 
-template <class T>
-T f(T i)
+int	main()
 {
-    static_assert(std::is_integral<T>::value, "Integral required.");
-    return i;
-}
- 
-#define SHOW(...) \
-    std::cout << std::setw(29) << #__VA_ARGS__ << " == " << __VA_ARGS__ << '\n'
- 
-int main()
-{
-    std::cout << std::boolalpha;
- 
-    SHOW( std::is_integral<A>::value );
-    SHOW( std::is_integral_v<E> );
-    SHOW( std::is_integral_v<float> );
-    SHOW( std::is_integral_v<int*> );
-    SHOW( std::is_integral_v<int> );
-    SHOW( std::is_integral_v<const int> );
-    SHOW( std::is_integral_v<bool> );
-    SHOW( std::is_integral_v<char> );
-    SHOW( std::is_integral_v<BF> );
-    SHOW( f(123) );
+	cout << boolalpha;
+
+	cout
+	<< "int: " << is_integral<int>::value << nl
+	<< "longint: " << is_integral<long int>::value << nl
+	<< "bool: " << is_integral<bool>::value << nl
+	<< "char: " << is_integral<char>::value << nl
+	<< "wchar_t: " << is_integral<wchar_t>::value << nl
+	<< "unsigned short int: " << is_integral<unsigned short int>::value << nl2;
+
+	cout
+	<< "float: " << is_integral<float>::value << nl
+	<< "double: " << is_integral<double>::value << nl
+	<< "long double: " << is_integral<long double>::value << nl2;
+
+	cout
+	<< "... \n"
+	<< "Built-in types (also called fundamental types) " 
+	<< "are specified by the C++ language standard and "
+	<< "are built into the compiler. \n"
+	<< "Built-in types aren't defined in any header file. \n\n"
+	<< "Built-in types are divided into three main categories: \n"
+	<< "integral, floating-point, and void. \n\n"
+	<< "Integral types represent whole numbers. \n"
+	<< "Floating-point types specify values w/ or w/o fractional parts. \n"
+	<< "Most built-in types are treated as distinct types by the compiler. \n"
+	<< "Some types are synonyms, "
+	<< "or treated as equivalent types by the compiler. \n"
+	<< "... \n\n";
 }
