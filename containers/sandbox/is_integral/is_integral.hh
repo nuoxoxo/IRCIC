11a126318f628f6f57c3be1c2c8f6d390d5fcb78
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-02-13 23:25:04 +0100

    halfway in is integralHH

--- containers/sandbox/is_integral/is_integral.hh
+++ containers/sandbox/is_integral/is_integral.hh
@@ -3,6 +3,16 @@
 
 namespace ft
 {
-	template<class T, T b>
-	class	integral
+	template<class T, T v>
+	class	integral_constant
+	{
+	public:
+		typedef integral_constant<T, v>	type;
+		typedef	T		value_type;
+		static const T		value = v;
+
+		operator T() { return v; }
+	}
+
+
 }
