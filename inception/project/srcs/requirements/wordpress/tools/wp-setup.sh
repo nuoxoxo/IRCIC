b577ca421a28a86a4fc68675cb85605126fc1c12
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-02-15 21:42:16 +0100

    :D del colleague's name

--- inception/project/srcs/requirements/wordpress/tools/wp-setup.sh
+++ inception/project/srcs/requirements/wordpress/tools/wp-setup.sh
@@ -17,7 +17,7 @@ else
 		
 	wp core install \
 		--url="$DOMAIN_NAME" \
-		--title="qxia inception" \
+		--title="inception" \
 		--admin_user="$WP_ADMIN" \
 		--admin_password="$WP_ADMIN_PASSWORD" \
 		--admin_email="$WP_ADMIN_MAIL" \
