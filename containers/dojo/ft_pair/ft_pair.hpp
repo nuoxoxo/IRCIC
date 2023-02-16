1b966ee7b7dabce3a267f8ec4a1604957a79ecc5
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-02-16 22:25:42 +0100

    primary try

--- containers/dojo/ft_pair/ft_pair.hpp
+++ containers/dojo/ft_pair/ft_pair.hpp
@@ -10,8 +10,22 @@ namespace	ft
 		T_2	second;
 	public:
 		pair() : first(), second() {}
+		~pair() {}
+		
 		pair(const T_1 & l, const T_2 & r) : first(l), second(r) {}
+		
+		template<typename L, typename R>
 		pair(const pair<L, R> & p) : first(p.first), second(p.second) {}
+
+		pair & operator = (const pair & p)
+		{
+			if (this != & dummy)
+			{
+				first = dummy.first;
+				second = dummy.second;
+			}
+			return (*this);
+		}
 	}
 
 	// operators
