dd91437c90dfe40052c0d0d9061b294d7b4dbe39
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-02-23 11:59:36 +0100

    + explicit conversion comparison

--- containers/sandbox/conversion___implicit_conversion/main.cc
+++ containers/sandbox/conversion___implicit_conversion/main.cc
@@ -1,21 +1,19 @@
 #include "iostream"
 #include "string"
 
-# define _c1	"\x1b[33m"
-# define _c2	"\x1b[36m"
-# define _r	"\x1b[0m"
-# define nl2	"\n\n"
+# define color_1 "\x1b[33m"
+# define color_2 "\x1b[36m"
+# define color_3 "\x1b[0m"
+# define nl2 "\n\n"
 
 class	Entity
 {
-	public:
-		std::string	Name;
-		int		Size;
+public:
+	std::string	Name;
+	int		Size;
 
-		Entity(const std::string & name)
-			: Name(name), Size(-1) {}
-		Entity(const int size)
-			: Name("Unknown"), Size(size) {}
+	Entity(const std::string & name) : Name(name), Size(-1) {}
+	Entity(const int size) : Name("Unknown"), Size(size) {}
 };
 
 void	printer(const Entity & e1, const Entity & e2)
@@ -33,9 +31,9 @@ int	main()
 
 	//	test 1
 
-	std::cout << _c2 "usual call: " nl2 _c1
+	std::cout << color_2 "usual call: " nl2 color_1
 	"Entity a(\"Monster\"); \n"
-	"Entity b(96883); " _r nl2;
+	"Entity b(96883); " color_3 nl2;
 
 	Entity	a("Monster");
 	Entity	b(196883);
@@ -44,9 +42,9 @@ int	main()
 
 	//	test 2
 
-	std::cout << _c2 "usual call: " nl2 _c1
+	std::cout << color_2 "usual call: " nl2 color_1
 	"Entity i = Entity(\"Monster\");\n"
-	"Entity o = entity(96883); " _r nl2;
+	"Entity o = entity(96883); " color_3 nl2;
 
 	Entity	i = Entity("Monster");
 	Entity	o = Entity(196883);
@@ -55,9 +53,9 @@ int	main()
 
 	//	test 3
 
-	std::cout << _c2 "implicit construction: " nl2 _c1
+	std::cout << color_2 "implicit construction: " nl2 color_1
 	"Entity x = string(\"Monster\"); \n"
-	"Entity y = 96883; " _r nl2;
+	"Entity y = 96883; " color_3 nl2;
 
 	Entity	x = std::string("Monster"); // can't do: Entity	x = "Monster";
 	Entity	y = 196883;
@@ -67,8 +65,8 @@ int	main()
 	//	test 3
 
 	std::cout <<
-	_c2 "implicit conversion: " nl2
-	_c1 "printer(std::string(\"Monster\"), 196883); " _r nl2;
+	color_2 "implicit conversion: " nl2
+	color_1 "printer(std::string(\"Monster\"), 196883); " color_3 nl2;
 
 	printer(std::string("Monster"), 196883);
 
