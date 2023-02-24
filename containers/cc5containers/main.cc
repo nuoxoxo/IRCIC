b55dedb58aa9a6a56479524e078b57c49e9d745c
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-02-25 00:07:22 +0100

    init main . to include tests per Stack & Pair

---
+++ containers/cc5containers/main.cc
@@ -0,0 +1,34 @@
+#include "iostream"
+
+
+
+/********************************************
+*  the main should test the following shit  *
+*********************************************/
+
+/* pair */
+
+// = . assign contents
+// make_pair()
+//  ==
+//  != 
+//  < . <=
+//  > . >=
+
+
+/* stack */
+
+// = . assign value to container adaptor
+//  empty
+//  size
+//  top
+//  push
+//  pop
+//  XXX emplace . constructs element in-place at the top
+// Member objects: Container C . underlying container
+//  ==
+//  != 
+//  < . <=
+//  > . >=
+
+
