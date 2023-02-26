1b45de146e1afcf52b89eaa391adc23bb155125b
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-02-26 11:05:52 +0100

    stack : test moved from main

--- containers/project/stack.hpp
+++ containers/project/stack.hpp
@@ -8,6 +8,7 @@
 
 namespace	ft
 {
+	// to be stuffed
 }
 
 template<typename T>
@@ -22,5 +23,62 @@ void	printer_cleaner_stack(T & st)
 	}
 	std::cout << "(size : " << GREEN << st.size() << RESET << ") \n\n";
 }
+
+// tests to call in main
+
+/********************************************
+*  the main should test the following shit  *
+*********************************************/
+
+// = . assign value to container adaptor
+//  empty
+//  size
+//  top
+//  push
+//  pop
+// Member objects: Container C . underlying container
+//  ==
+//  != 
+//  < . <=
+//  > . >=
+//  emplace . constructs element in-place at the top . XXX
+
+# include "stack"
+void	stack_test(void)
+{
+	std::cout << GREEN "\npush pop empty size" nl2reset;
+
+	int	i = -1;
+
+	std::cout << LOWKEY "\nTest " << ++i << " :: stack<int>"nl2reset;
+	{
+		std::stack<int>	S;
+		S.push(41);
+		S.push(42);
+		S.push(43);
+		printer_cleaner_stack(S);
+		// S.pop(); // ----> should segfault with std::stack
+	}
+	std::cout << LOWKEY "\nTest " << ++i << " :: stack<string>"nl2reset;
+	{
+		std::stack<std::string>	S;
+		S.push("= = = = = = = = = = = = = world");
+		S.push("= = = = = = = = = = hello ");
+		S.push("= = = = = = =");
+		S.push("= = = =");
+		S.push("= ");
+		printer_cleaner_stack(S);
+	}
+	std::cout << LOWKEY "\nTest " << ++i << " :: stack<double>"nl2reset;
+	{
+		std::stack<double>	S;
+		S.push(21.21);
+		S.push(42.43);
+		S.push(196.883);
+		printer_cleaner_stack(S);
+	}
+}
+
+
 
 #endif
