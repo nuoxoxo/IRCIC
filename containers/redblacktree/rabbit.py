a6aece4812451904935c231bc13f77e44b562dba
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-02-26 23:02:53 +0100

    insertion : todo

--- containers/redblacktree/rabbit.py
+++ containers/redblacktree/rabbit.py
@@ -1,8 +1,25 @@
-class rbtnode:
+class treenode :
     def __init__(self, val) :
         self.parent = None
         self.red = False # ie. black
         self.left = None
         self.right = None
         self.val = val
+
+class tree :
+    def __init__(self) :
+        self.nil.red = False # ie. black
+        self.nil.left = None
+        self.nil.right = None
+        self.nil = ndoe(0)
+        self.root = self.nil
+
+def insert(self, val):
+    # class bst insertion
+
+
+
+
+
+
 
