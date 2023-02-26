1a3e919b05ffdedf92038544b9c68bd13fe9921d
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-02-26 11:04:40 +0100

    main . replace tests

--- containers/project/main.cc
+++ containers/project/main.cc
@@ -2,56 +2,10 @@
 #include "colors.hpp"
 #include "pair.hpp"
 #include "stack.hpp"
-#include "stack" // - to delete
 
 int	main()
 {
-	// {pair_test();} // hey
-	{
-		std::stack<int>	S;
-		S.push(41);
-		S.push(42);
-		S.push(43);
-		printer_cleaner_stack(S);
-	}
-	{
-		std::stack<std::string>	S;
-		S.push("= = = = = = = = = = = = =");
-		S.push("= = = = = = = = = = ");
-		S.push("= = = = = = =");
-		S.push("= = = =");
-		S.push("= ");
-		printer_cleaner_stack(S);
-	}
+	pair_test();
+	stack_test();
+		
 }
-
-/********************************************
-*  the main should test the following shit  *
-*********************************************/
-
-/* stack */
-
-// = . assign value to container adaptor
-//  empty
-//  size
-//  top
-//  push
-//  pop
-// Member objects: Container C . underlying container
-//  ==
-//  != 
-//  < . <=
-//  > . >=
-//  emplace . constructs element in-place at the top . XXX
-
-
-/* pair */
-
-// = . assign contents
-// make_pair()
-//  ==
-//  != 
-//  < . <=
-//  > . >=
-
-
