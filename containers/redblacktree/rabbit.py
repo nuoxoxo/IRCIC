3ea76f35e44573ea0f4a17d1dcfdefd86e9ec30d
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-02-27 23:00:59 +0100

    fi. Fix func

--- containers/redblacktree/rabbit.py
+++ containers/redblacktree/rabbit.py
@@ -16,15 +16,38 @@ class tree :
 
     def Fix(self, node):
         while node != self.root and node.parent.red == True:
-            uncle = node.parent.parent.left
             
-            if uncle.red == True:
-                uncle.red = False
-                node.parent.red = False
-                node.parent.parent.red = True
-                node = node.parent.parent
+            if node.parent == node.parent.parent.right: # if parent is a R node
+                uncle = node.parent.parent.left
+                
+                if uncle.red == True:
+                    uncle.red = False
+                    node.parent.red = False
+                    node.parent.parent.red = True
+                    node = node.parent.parent
+                else:
+                    if node == node.parent.left:
+                        node = node.parent
+                        self.Rotate_Right(node) # Rotate Right
+                    node.parent.red = False
+                    node.parent.parent.red = True
+                    self.Rotate_Left(node.parent.parent) # Rotate Left
             else:
-                if # ...
+                uncle = node.parent.parent.right
+
+                if uncle.red == True:
+                    uncle.red = False
+                    node.parent.red = False
+                    node.parent.parent.red = True
+                    node = node.parent.parent
+                else:
+                    if node == node.parent.right:
+                        node = node.parent
+                        self.Rotate_Left(node) # Rotate Left
+                    node.parent.red = False
+                    node.parent.parent.red = True
+                    self.Rotate_Right(node.parent.parent)
+        self.root.red = False
 
     def Insert(self, val):
 
