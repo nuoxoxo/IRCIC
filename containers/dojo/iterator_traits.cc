53a94bc99a6d87f48de5a59e578bc88b6b8b463d
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-03-01 14:26:34 +0100

    init iterator - added std impl.

--- containers/dojo/iterator_traits.cc
+++ containers/dojo/iterator_traits.cc
@@ -1,18 +1,35 @@
 #ifndef __ITERATOR_TRAITS__
 # define __ITERATOR_TRAITS__
 
-namespace	ft
+/*
+namespace std  {
+	template <class T>
+	struct	iterator_traits {
+		typedef typename T::value_type		value_type;
+		typedef typename T::difference_type	difference_type;
+		typedef typename T::iterator_category	iterator_category;
+		typedef typename T::pointer		pointer;
+		typedef typename T::reference		reference;
+	};
+}
+*//* std::iterator_traits 
+     is the type trait class that provides 
+     uniform interface to the properties
+     of LegacyIterator types. */
+
+namespace	ft /*std*/
 {
 
-template<typename T>
+template<typename /*class*/ T>
 class	iterator_traits
 {
+public:
 	typedef	typename T::iterator_category	iterator_category;
 	typedef	typename T::difference_type	difference_type;
 	typedef	typename T::value_type		value_type;
 	typedef	typename T::pointer		pointer;
 	typedef	typename T::reference		reference;
-}
+};
 
 }
 
