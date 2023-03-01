10bad18c454d9b41d47e6e6107186cf4ce6507c4
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-03-01 15:08:26 +0100

    iterator : + unordered_map

--- containers/sandbox/distance/main.cc
+++ containers/sandbox/distance/main.cc
@@ -2,6 +2,7 @@
 #include "algorithm"
 #include "vector"
 #include "map"
+#include "unordered_map"
 #define Size 42
 #define vi vector<int>
 #define mii map<int, int>
@@ -11,7 +12,10 @@ using	namespace std;
 
 void	pv(vi &);
 void	pmii(mii &);
-void	mapfill(mii &);
+void	pmii(umii &); // ovld
+
+template<class T>
+void	mapfill(T &);
 
 int	main()
 {
@@ -27,35 +31,43 @@ int	main()
 	cout << "Size of map: " << distance(m.begin(), m.end()) << endl;
 	pmii(m);
 
-	/* impl. needed for unordered_map */
-	/*
-	mii	m;
-	mapfill(m);
-	cout << "Size of map: " << distance(m.begin(), m.end()) << endl;
-	pmii(m);
-	*/
+	umii	u;
+	mapfill(u);
+	cout << "Size of map: " << distance(u.begin(), u.end()) << endl;
+	pmii(u);
 }
 
-void	pv(vi & v)
+void	pv(vi & v) // 
 {
 	vi::iterator it = v.begin();
 	int	i = 0;
 	while (it != v.end())
 		i = (i%10)?i+1:1, cout << *it << ((i%10)? ' ':'\n'), ++it;
-	cout << '\n';
+	cout << "\n\n";
 }
 
-void	pmii(mii & m)
+void	pmii(mii & m) // printer for map<int,int>
 {
 	mii::iterator it = m.begin();
 	int	i = 0;
 	while (it != m.end())
 		i = (i%4)?i+1:1, cout << it->first << " - " << it->second \
 		    << ((i%4)? "\t\t":"\n"), ++it;
-	cout << '\n';
+	cout << "\n\n";
 }
 
-void	mapfill(mii & m)
+void	pmii(umii & u) // printer for unordered_map<int,int>
+{
+	umii::iterator it = u.begin();
+	int	i = 0;
+	while (it != u.end())
+		i = (i%4)?i+1:1, cout << it->first << " - " << it->second \
+		    << ((i%4)? "\t\t":"\n"), ++it;
+	cout << "\n";
+}
+
+template<class T>
+void	mapfill(T & m)
 {
 	int	i = -1;
 	while (++i < Size)
