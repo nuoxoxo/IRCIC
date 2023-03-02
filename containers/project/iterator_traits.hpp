7146e5765c880a0c269638790dc87509371f24d9
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-03-02 21:52:31 +0100

    iterator : traits for actual STL

--- containers/project/iterator_traits.hpp
+++ containers/project/iterator_traits.hpp
@@ -6,14 +6,34 @@
 
 namespace	ft
 {
-	// 5 Iter categories
-	// defined for STL in file named : stl_iterator_base_types.hpp
+	// 5 Iter categories defined for STL in file named : 
+	// 	stl_iterator_base_types.hpp
 
-	struct	input_iterator_tag {};
-	struct	output_iterator_tag {};
-	struct	forward_iterator_tag : public input_iterator_tag {};
-	struct	bidirectional_iterator_tag : public forward_iterator_tag {};
-	struct	random_access_iterator_tag : public bidirectional_iterator_tag {};
+	/**
+	 * @defgroup iterator_tags Iterator Tags
+	 * These are empty types, used to distinguish different iterators.  The
+	 * distinction is not made by what they contain, but simply by what they
+	 * are.  Different underlying algorithms can then be used based on the
+	 * different operations supported by different iterator types.
+	 */
+
+	///  Marking input iterators.
+	struct input_iterator_tag {};
+
+	///  Marking output iterators.
+	struct output_iterator_tag {};
+
+	/// Forward iterators support a superset of input iterator operations.
+	struct forward_iterator_tag : public input_iterator_tag {};
+
+	/// Bidirectional iterators support a superset of forward iterator
+	/// operations.
+	struct bidirectional_iterator_tag : public forward_iterator_tag {};
+
+	/// Random-access iterators support a superset of bidirectional iterator
+	/// operations.
+	struct random_access_iterator_tag : public bidirectional_iterator_tag {};
+
 
 	template<typename T>
 	class	iterator_traits
