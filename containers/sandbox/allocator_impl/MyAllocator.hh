0379ca7a1521da4a3b4e231adffaa6539659cd52
nuoxoxo <nuxu@e1r5p10.clusters.42paris.fr>
2023-03-03 13:10:04 +0100

    where do we alloc exactly

--- containers/sandbox/allocator_impl/MyAllocator.hh
+++ containers/sandbox/allocator_impl/MyAllocator.hh
@@ -1,5 +1,7 @@
 /* 
- * < The C++ Standard Library - A Tutorial and Reference >
+ * sample impl. by josuttis
+ *
+ * The C++ Standard Library - A Tutorial and Reference
  * Nicolai M. Josuttis, Addison-Wesley, 1999
  *
  */
@@ -31,6 +33,7 @@ namespace	MyLibrary
 		typedef T		value_type;
 		typedef std::size_t	size_type;
 		typedef std::ptrdiff_t	difference_type;
+
 		typedef T *		pointer;
 		typedef T &		reference;
 		typedef const T *	const_pointer;
@@ -66,20 +69,23 @@ namespace	MyLibrary
 
 
 
-		// return maximum number of elements that can be allocated
-		size_type max_size () const throw()
+		// max number of elements we can allocate
+
+		size_type	max_size () const throw()
 		{ return std::numeric_limits<std::size_t>::max() / sizeof(T); }
 
 
-		// allocate but don't initialize num elements of type T
-		pointer	allocate (size_type num, const void * = 0)
+		// allocate n elements of type T
+		// but don't initialize any value
+
+		pointer	allocate(size_type n, const void * = 0)
 		{
 			// print message and allocate memory with global new
 			std::cerr
-			<< "allocate " << num << " element(s)"
+			<< "allocate " << n << " element(s)"
 			<< " of size " << sizeof(T) << std::endl;
 
-			pointer	res = (pointer) (::operator new (num * sizeof(T)));
+			pointer	res = (pointer) (::operator new (n * sizeof(T)));
 
 			std::cerr << MSGALLOC "at: " << (void *) res << nl2;
 
@@ -88,22 +94,26 @@ namespace	MyLibrary
 
 
 		// initialize elements of allocated storage p with value value
-		void	construct (pointer p, const T & value) { new((void *) p) T(value); }  // initialize memory with placement new
+		//  - initialize memory with placement new
+		void	construct(pointer p, const T & val)
+		{ new((void *) p) T(val); }  
 
 
 		// destroy elements of initialized storage p
-		void	destroy (pointer p) { p->~T(); } // destroy objects by calling their destructor
+		//  - destroy objects by calling their destructor
+		void	destroy(pointer p) { p->~T(); }
 
 
 		// deallocate storage p of deleted elements
-		void	deallocate (pointer p, size_type num) //deallocate memory with global delete
+		//  - deallocate memory with global delete
+		void	deallocate(pointer p, size_type n)
 		{
 			std::cerr
-			<< "deallocate " << num << " element(s)"
+			<< "deallocate " << n << " element(s)"
 			<< " of size " << sizeof(T) << std::endl
-			<< MSGDEALLOC "at: " << (void*)p << nl2;
+			<< MSGDEALLOC "at: " << (void *) p << nl2;
 
-			::operator delete((void*)p); // del.
+			::operator delete((void *) p); // del.
 		}
 
 
@@ -113,10 +123,12 @@ namespace	MyLibrary
 	// return that all specializations of this allocator are interchangeable
 
 	template<class T1, class T2>
-	bool	operator == (const MyAllocator<T1> &, const MyAllocator<T2> &) throw() { return true; }
+	bool	operator == (const MyAllocator<T1> &, const MyAllocator<T2> &) 
+		throw() { return true; }
 
 	template<class T1, class T2>
-	bool	operator != (const MyAllocator<T1> &, const MyAllocator<T2> &) throw() { return false; }
+	bool	operator != (const MyAllocator<T1> &, const MyAllocator<T2> &)
+		throw() { return false; }
 }
 
 #endif
