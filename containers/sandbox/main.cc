e3dcd1b301e67191e53c578757bafb3e71460ab4
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-03-07 22:58:27 +0100

    sandbox : assign

---
+++ containers/sandbox/main.cc
@@ -0,0 +1,22 @@
+#include "iostream"
+#include "vector"
+
+#define nl "\n"
+
+using	namespace std;
+
+int	main ()
+{
+	vector<int>	A, B, C;
+	int		arr[] = { 21, 42, 196883, 22, 43, 1024 };
+
+	A.assign(7, 42); // size: 7
+
+	B.assign(A.begin() + 1, A.end() - 1); // size: 5
+
+	C.assign(arr, arr + 3); // 3
+
+	cout << "Size of A: " << A.size() << nl;
+	cout << "Size of B: " << B.size() << nl;
+	cout << "Size of B: " << C.size() << nl;
+}
