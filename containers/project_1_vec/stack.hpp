900cc638e4de5af35d3e307426be34337f210d2e
qxqxia <96128825+qxqxia@users.noreply.github.com>
2023-03-08 11:48:30 +0100

    Update stack.hpp

--- containers/project_1_vec/stack.hpp
+++ containers/project_1_vec/stack.hpp
@@ -13,6 +13,7 @@
 namespace	ft
 {
 	template < typename T, typename Container = std::vector<T> > // std //XXX
+	template < typename T, typename Container = ::vector<T> > // ft
 	class	stack
 	{
 	// protected:
