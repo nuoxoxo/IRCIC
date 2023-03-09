fdfe4673c940f6884fc0361d079491cb8c3686e4
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-03-09 17:09:07 +0100

    init : rbt as util

--- containers/project_2_map/util_rbtree.hpp
+++ containers/project_2_map/util_rbtree.hpp
@@ -1,1 +1,19 @@
-#ifndef ____
+#pragma once
+#ifndef __UTIL_RBTREE_HPP__
+# define __UTIL_RBTREE_HPP__
+
+// function && memory :: obsolete on mac // FIXME
+
+# include "iterator_rbtree_iterator.hpp"
+# include "pair.hpp"
+# include "util_swap.hpp"
+# include "util_rbtree.hpp"
+# include "util_lexicographical_compare.hpp"
+
+namespace	ft
+{
+	// make stuff up
+}
+
+#endif
+
