9be617cd8e262f05a44d53cdfa6cc356ee1afa20
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-03-09 17:27:14 +0100

    mapbox : init

---
+++ containers/sandbox/map/README.md
@@ -0,0 +1,6 @@
+# __Updates__
+- Mar 9
+- Mar 8
+  - created symlinks
+
+![](https://i.imgur.com/bpr8tqf.jpg)
---
+++ containers/sandbox/map/main.cc
@@ -0,0 +1,24 @@
+#include "iostream"
+
+#include "pair.hpp"
+#include "stack.hpp"
+#include "vector.hpp"
+// #include "map.hpp" // <---
+#include "colors.hpp"
+
+#include "cstdlib" // random
+#include "vector" // for reproducing mazoise
+
+int	main()
+{
+	int	i = 0;
+
+	srand(time(0));
+
+	print_session_head(++i, "swap test :: vector<int>");
+
+	{
+		// ft::map<int, int>
+	}
+
+}
