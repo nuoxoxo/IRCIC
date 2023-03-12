efc1179e6315f385b40796e1c0b1080f77f93dd6
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-03-12 10:48:46 +0100

    box : + fmt hpp

---
+++ containers/sandbox/Fmt.hpp
@@ -0,0 +1,19 @@
+#define Y "\033[0;32m"
+#define C "\033[0;36m"
+#define R "\033[0m"
+
+#define nl2 "\n\n"
+#define nl "\n"
+#define nl2reset nl2 R
+#define nlreset nl R
+
+#define YELLOW "\033[0;33m"
+#define CYAN "\033[0;36m"
+#define GREEN "\033[0;32m"
+#define RESET "\033[0m"
+
+#define YELLOW_BOLD "\033[1;33m"
+#define CYAN_BOLD "\033[1;36m"
+#define GREEN_BOLD "\033[1;32m"
+#define RED_BOLD "\033[1;31m"
+
--- containers/sandbox/sfinae_eli2/main.cc
+++ containers/sandbox/sfinae_eli2/main.cc
@@ -1,39 +1,13 @@
 #include "iostream"
+#include "../Fmt.hpp"
+
 using	namespace std;
 
-#define Y "\033[0;32m"
-#define C "\033[0;36m"
-#define R "\033[0m"
-#define nl2 "\n\n"
-#define nl "\n"
-#define nl2reset nl2 R
-#define nlreset nl R
-
-
-//	Funciton 1
-
-//void	func(unsigned int i) { cout << Y << "uint: " << i << nl2reset; }
-void	func(unsigned int i) {
-	cout << Y << "uint: " << i << nlreset;
-	cout << "(type: " << typeid(i).name() << ")" << nl2;
+int	negate(int i) {
+ 	return -i;
 }
-
-//	Funciton 2: template
 
 template<typename T>
-// void	func(const T & t) { cout << C << "template: " << t << nlreset; }
-void	func(const T & t) {
-	cout << C << "template: " << t << nlreset;
-	cout << "(type: " << typeid(t).name() << ")" << nl2;
+typenameT::value_type negate(const T& t) {
+  return -T(t);
 }
-
-
-int	main()
-{
-	func(42); // int => signed by default
-	func((uint8_t) 42); // ascii char
-	func((uint32_t) 42); // // normal
-	func((unsigned int) 42); // idem
-}
-
-// ru
