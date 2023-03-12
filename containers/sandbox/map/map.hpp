3c38e4c2e6f1aac43f00b0fea69d4e2401d95d61
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-03-12 23:42:37 +0100

    map : re init boilerplate

--- containers/sandbox/map/map.hpp
+++ containers/sandbox/map/map.hpp
@@ -1,15 +1,21 @@
 #ifndef __MAP_HPP__
 # define __MAP_HPP__
 
-// function && memory :: obsolete on mac // FIXME
-
 #include "iostream"
 #include "pair.hpp"
-#include "red_black_tree.hpp"
 #include "util_enable_if.hpp"
 #include "util_is_integral.hpp"
 #include "iterator_reverse_iterator.hpp"
+#include "red_black_tree.hpp" // incl. rbtIter
 
+namespace ft
+{
+	template <class Key, class T, class Compare = std::less<Key>, class Allocator = std::allocator<ft::pair<const Key, T> > >
+	class map
+	{
+	public :
+	};
+}
 
 #endif
 
