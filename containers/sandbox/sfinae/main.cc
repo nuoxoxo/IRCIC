267edfb85b6e1094f1481351d0e88f47f441e768
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-03-12 02:16:10 +0100

    sandbox : init sfinae

---
+++ containers/sandbox/sfinae/main.cc
@@ -0,0 +1,24 @@
+#include "iostream"
+
+using namespace std;
+
+class	X {};
+struct	Y { using type = X; };
+
+template<class T>
+void	print(T)
+{
+	cout << __FUNCTION__ << " - func(T)" << endl;
+}
+
+template<class T>
+void	print(typename T::type)
+{
+	cout << __FUNCTION__ << " - func(T::type)" << endl;
+}
+
+int	main()
+{
+	print<X>(X{});
+	print<Y>(X{});
+}
