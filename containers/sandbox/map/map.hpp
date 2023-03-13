8c020bf40ac3481f4bf9b59cc41ac6f70aa4e4f4
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-03-13 21:54:46 +0100

    map : working on hpp

--- containers/sandbox/map/map.hpp
+++ containers/sandbox/map/map.hpp
@@ -10,12 +10,37 @@
 
 namespace ft
 {
-	template <class Key, class T, class Compare = std::less<Key>, class Allocator = std::allocator<ft::pair<const Key, T> > >
+	template<class Key, class T, class Compare = std::less<Key>,
+	class Allocator = std::allocator<ft::pair<const Key, T> > >
 	class map
 	{
 	public :
 	};
+
+	template<class Key, class T, class key_compare, class Alloc>
+	bool operator == (
+		const map<Key, T, key_compare, Alloc> & L,
+		const map<Key, T, key_compare, Alloc> & R
+	)
+	{
+		typename map<Key, T, key_compare, Alloc>::const_iterator it, it2;// = L .begin();
+
+		L = L.begin();
+		R = R.begin();
+		if (L .size() != R .size())
+			return false;
+		while (it != L .end())
+		{
+			if (*it != *it2)
+				return false;
+			it++;
+			it2++;
+		}
+		return true;
+	}
+
 }
+
 
 #endif
 
