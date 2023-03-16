94903dbf1b673dfed952a9fad85d2733ef6022f2
qxqxia <qxqinxia@outlook.com>
2023-03-16 14:18:16 +0100

    fix discord test

--- module9/ex01/main.cc
+++ module9/ex01/main.cc
@@ -25,11 +25,12 @@ int	main(int c, char **v)
 	test("8 9 * 9 - 9 - 9 - 4 - 1 +", "42");
 	test("7 7 * 7 -", "42");
 	test("(1 + 1)", "Error");
-	test("10 6 9 3 + -11 * / * 17 + 5 +", "22");
+	test("10 6 9 3 + -11 * / * 17 + 5 +", "13"/* "22" */); // gfg
 
 	test("3 4 +", "7");
 	test("3 5 6 + *", "33");
-	test("3 10 5 + *", "45");
+	// test("3 10 5 + *", "45");
+	test("12 * 2 / 5 + 46 * 6 / 8 * 2 / + 2 * 2 -", "42");
 }
 
 
@@ -54,7 +55,7 @@ void	test(string expr, string compare)
 
 //to_string not include in c++98
 template<typename T>
-std::string to_string(const T & value) {
+string to_string(const T & value) {
     std::ostringstream oss;
     oss << value;
     return oss.str();
@@ -67,9 +68,12 @@ string	calc(string line)
 	stringstream	ss(line);
 	while (!ss.eof() && ss >> s)
 	{
+
 		if (s == "+" || s == "-" || s == "*" || s == "/")
 		{
 			int	r = 0, l = 0;
+			// if (s == "*" || s == "/")
+			// {r = 1, l = 1;}
 			if (E.size())
 			{
 				stringstream(E.top()) >> r;
@@ -80,6 +84,7 @@ string	calc(string line)
 				stringstream(E.top()) >> l;
 				E.pop();
 			}
+			// cout << l << " - " << r << " - " << s << endl;
 			if (s == "+")
 				E.push(to_string(l + r));
 			else if (s == "-")
@@ -98,7 +103,42 @@ string	calc(string line)
 				return "Error";
 		}
 		else if (isnumeric(s))
-			E.push(s);
+		// {
+		// 	E.push(s);
+		// }
+		{
+			long long ll;
+			stringstream(s) >> ll;
+			if (ll < -2147483648)
+				return "Error";
+			if (ll > 9)
+			{
+				stack<int>	temp1;
+				while (ll)
+				{
+					int n = ll % 10;
+					ll /= 10;
+					temp1.push(n);
+				}
+				stack<int>	temp2;
+				while (!temp1.empty())
+				{
+					int n = temp1.top();
+					temp2.push(n);
+					temp1.pop();
+				}
+				while (!temp2.empty())
+				{
+					int n = temp2.top();
+					temp2.pop();
+					E.push(to_string(n));
+				}
+			}
+			else
+			{
+				E.push(s);
+			}
+		}
 	}
 	return E.top();
 }
