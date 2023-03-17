323a8d1be7e088eb084893b3af26558fb4009caa
uxn <malcolm.nuo.xu@gmail.com>
2023-03-17 10:32:24 +0100

    x0 : fi.

--- module9/ex00/BitcoinExchange.hpp
+++ module9/ex00/BitcoinExchange.hpp
@@ -2,6 +2,13 @@
 #ifndef __BITCOINEXCHANGE_HPP__
 # define __BITCOINEXCHANGE_HPP__
 
+# include "iostream"
+# include "fstream"
+# include "sstream"
+# include "cstdlib"
+# include "cstdio"
+# include "map"
+
 # define nl "\n"
 # define nl2 "\n\n"
 # define nlreset " \n" RESET
@@ -15,8 +22,25 @@
 # define RESET "\033[0;0m"
 # define ITAL "\033[3m"
 
+#define COLOR RED
+
 # define TICK	GREEN " ✓" RESET
 # define CROSS	_RED_ " ✘" RESET
+
+template<typename T>
+    std::string to_string(const T value);
+
+std::string to_string(float const);
+
+float	to_float_round_2(float);
+float	to_float_floor_2(float);
+bool	_check_params_(int, char **);
+void	printerr(std::string s = "");
+bool	date_is_valid(std::string &);
+bool	query_is_valid(std::string &);
+bool	title_is_valid(std::string &);
+bool	isnumeric(std::string &);
+bool	number_check(std::string &);
 
 #endif
 
