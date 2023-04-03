2e48b432c775a54e3d1d875a9ee2ed91f1f4f098
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-04-03 19:13:11 +0200

    copy to drive

---
+++ event_april_2023/driver/ex11/find.py
@@ -0,0 +1,20 @@
+import json
+import requests
+
+
+p = input('Enter the name of a Pokemon: ')
+
+response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{p.lower()}')
+# print(response.status_code)
+
+if response.status_code == 404:
+    print('connection error. please verify your choice of pokemon. ')
+    exit()
+res = response.json()
+#print(type(res))
+
+print('Abilities:')
+for key in res['abilities']:
+    print('-', key['ability']['name'])
+
+
