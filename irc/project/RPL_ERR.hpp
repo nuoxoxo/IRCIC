1d828ac2fdd7e5afe3e9aa7dd1bc01ade7fd116c
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-05-14 23:35:52 +0200

    RPL ERR hpp

--- irc/project/RPL_ERR.hpp
+++ irc/project/RPL_ERR.hpp
@@ -4,13 +4,13 @@
 
 # include "Freenode.hpp"
 
-std::string get_RPL_ERR(int code, Server *serv, User *user, std::string arg, std::string arg2);
+std::string Get_RPL_ERR(int code, Server *serv, User *user, std::string arg, std::string arg2);
 
 //RPL
 
 //001
 # define RPL_WELCOME(n, u, h) \
-    (":\033[0;32mWelcome to the <FreeNode/Discord> Network " + n + "[!" + u + "@" + h + "]" + RESET)
+    (":" GREEN "Welcome to the <FreeNode/Discord> Network " + n + "[!" + u + "@" + h + "]" RESET)
 //    "<client> :Welcome to the <networkname> Network, <nick>[!<user>@<host>]"
 //    :server 001 <nick> :Welcome to the <network> Network, <nick>[!<user>@<host>]
 
@@ -35,6 +35,17 @@ std::string get_RPL_ERR(int code, Server *serv, User *user, std::string arg, std
 
 //221
 # define RPL_UMODEIS(arg) (arg)
+//  "<user mode string>"
+//  DOC ::
+
+/*
+221     RPL_UMODEIS
+        "<user mode string>"
+
+        - To answer a query about a client's own mode,
+            RPL_UMODEIS is sent back.
+*/
+
 
 //324
 # define RPL_CHANNELMODEIS(channel, modes) \
